42b5f0849bf7cdb4b8792974ed0b34f68af164bf
Pavlenko-Oksana <65184570+Pavlenko-Oksana@users.noreply.github.com>
2020-08-27 14:52:01 +0300

    добавляет решение задачи 4

---
+++ js/task-4-03.js
@@ -0,0 +1,28 @@
+const countTotalSalary = function (employees) {
+  "use strict";
+  // Write code under this line
+  const keys = Object.keys(employees);
+  let totalSalary = 0;
+
+  for (let key of keys) {
+    totalSalary += employees[key];
+  }
+  return totalSalary;
+};
+
+// Объекты и ожидаемый результат
+const developers = {
+  mango: 300,
+  poly: 250,
+  alfred: 450,
+};
+console.log(countTotalSalary(developers));
+// 1000
+
+const supports = {
+  kiwi: 200,
+  lux: 150,
+  chelsy: 150,
+};
+console.log(countTotalSalary(supports));
+// 500
